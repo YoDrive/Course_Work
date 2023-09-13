561acc5e43c69d5489fcc5397839fb5c990a7ef0
valjetaA <darjac6463@gmail.com>
2023-09-13 18:28:49 +0300

    test3

--- YoCars/Controllers/WeatherForecastController.cs
+++ YoCars/Controllers/WeatherForecastController.cs
@@ -25,9 +25,7 @@ public class WeatherForecastController : ControllerBase
         return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Id = Guid.NewGuid(),
-                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                TemperatureC = Random.Shared.Next(-20, 55),
-                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index))
             })
             .ToArray();
     }
