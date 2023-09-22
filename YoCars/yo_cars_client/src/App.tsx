65a400ef547a0948028fa1eefd0b94c50a5fca83
tanyginvv <tanyginvv@mail.ru>
2023-09-22 23:11:09 +0300

    pravki lk perenos header footer
    переименовал в лк всячину, вынес хедер и футер в app.tsx теперь надо хедер поправить

--- YoCars/yo_cars_client/src/App.tsx
+++ YoCars/yo_cars_client/src/App.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
 import './App.css';
 import HomePage from "./components/homePage/homePage";
-import PrivateOffice from './components/privateOffice/privateOffice';
+import  LKClient  from './components/privateOffice/lkClient';
 import {BrowserRouter, Routes,Route} from "react-router-dom";
+import Header from "../src/components/header/header"
+import Footer from "../src/components/footer/footer"
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <BrowserRouter>
         <Routes>
         <Route  path="/" element={<HomePage/>} />
-        <Route  path="/PrivateOffice" element={<PrivateOffice/>} />
+        <Route  path="/lkClient" element={<LKClient/>} />
         </Routes>
-        </BrowserRouter>
+      </BrowserRouter>
+      <Footer/>
     </div>
   );
 }
