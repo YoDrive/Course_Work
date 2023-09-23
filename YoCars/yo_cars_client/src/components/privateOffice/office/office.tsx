0fc600aef76173a18a001f852239788edb06384d
AsdZy <dmitry.ildyukov@mail.eu>
2023-09-23 13:12:39 +0300

    Маленькие доработки

--- YoCars/yo_cars_client/src/components/privateOffice/office/office.tsx
+++ YoCars/yo_cars_client/src/components/privateOffice/office/office.tsx
@@ -9,18 +9,20 @@ export function Office() {
         <div className={styles.container}>
             <div className={styles.content}>
                 <Header/>
-                <div className={styles.menu}>
-                    <div className={styles.menuHead}>
-                        <img className={styles.headIcon} src={User} alt=""></img>
-                        <p className={styles.headName}>Фамилия Имя Oтчество</p>
-                    </div>
-                    <div className={styles.menuButtons}>
-                        <button  className={styles.menuButton}>Мои данные</button>
-                        <button  className={styles.menuButton}>Редактировать профиль</button>
-                        <button  className={styles.menuButton}>История бронирований</button>
-                        <button  className={styles.menuButton}>Забронировать автомобиль</button>
-                        <button  className={styles.menuButton}>Настройки</button>
-                        <button  className={styles.menuButton}>Выход</button>
+                <div className={styles.mainPart}>
+                    <div className={styles.menu}>
+                        <div className={styles.menuHead}>
+                            <img className={styles.headIcon} src={User} alt=""></img>
+                            <p className={styles.headName}>Фамилия Имя Oтчество</p>
+                        </div>
+                        <div className={styles.menuButtons}>
+                            <button  className={styles.menuButton}>Мои данные</button>
+                            <button  className={styles.menuButton}>Редактировать профиль</button>
+                            <button  className={styles.menuButton}>История бронирований</button>
+                            <button  className={styles.menuButton}>Забронировать автомобиль</button>
+                            <button  className={styles.menuButton}>Настройки</button>
+                            <button  className={styles.menuButton}>Выход</button>
+                        </div>
                     </div>
                 </div>
             </div>
