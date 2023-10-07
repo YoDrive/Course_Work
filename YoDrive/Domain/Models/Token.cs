8d0820cc9689bc06524af6e767862a8ec463cd00
Дмитрий Ильдюков <dmitry.ildyukov@mail.ru>
2023-10-08 00:19:06 +0300

    Сделал миграцию

--- YoDrive/Domain/Models/Token.cs
+++ YoDrive/Domain/Models/Token.cs
@@ -1,7 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace YoDrive.Domain.Models;
 
 public class Token
 {
+    [Key]
+    [Required]
     public int UserId { get; set; }
     public string RefreshToken { get; set; }
 }
