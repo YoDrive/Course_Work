a1787eff270033f0ab758348ea8daf654e9166f3
tanyginvv <tanyginvv@mail.ru>
2023-10-14 11:44:51 +0300

    Доделал редактор чуть
    по кнопкам выпадает фильтр, добавил ховер эффекты, добавил кнопкам focus

--- yo_drive_client/src/components/privateOffice/office/editor.tsx
+++ yo_drive_client/src/components/privateOffice/office/editor.tsx
@@ -5,15 +5,24 @@ import { Prewiev } from './prewiev'
 import { Add } from './add'
 
 export function Editor(){
+    const[filter, setFilter]=useState(true);
     const[prewiev, setPrewiev] = useState(true);
     const[add, setAdd] = useState(false);
     const prewievHandler = () =>{
         setPrewiev(true);
         setAdd(false);
+        setFilter(true);
     }
     const addHandler = () =>{
         setAdd(true);
         setPrewiev(false);
+    }
+    const filterHandler = () =>{
+        setFilter(false);
+    }
+    const addFunc = () =>{
+        addHandler();
+        filterHandler();
     }
     return(
         <div className={styles.info}>
@@ -21,11 +30,11 @@ export function Editor(){
                 <h1 className={styles.infoHeader}>Редактор автомобилей</h1>
                 <div className={styles.headerButtons}>
                     <button onClick={prewievHandler} className={styles.buttonPrewiev}>Просмотреть список авто</button>
-                    <div className={styles.filter}>
+                    {filter &&<div className={styles.filter}>
                         <img className={styles.filterImg} src={vector}></img>
                         <button className={styles.filterButton}>Фильтр</button>
-                    </div>
-                    <button onClick={addHandler} className={styles.buttonAdd}>Добавить авто</button>
+                    </div>}
+                    <button onClick={addFunc} className={styles.buttonAdd}>Добавить авто</button>
                 </div>
             </div>
             {prewiev && <Prewiev/>}
