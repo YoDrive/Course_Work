c5b013e9daf4f2a8942a2fc43d3a317ac0d756e1
Daria Platonova <darjac6463@gmail.com>
2023-10-16 17:35:25 +0300

    Почистила

--- yo_drive_client/src/components/header/header.tsx
+++ yo_drive_client/src/components/header/header.tsx
@@ -7,60 +7,6 @@ import authorizationIcon from '../../assets/authorizationIcon.svg';
 import menuIcon from '../../assets/menuIcon.svg';
 import menuExit from '../../assets/menuExit.svg'
 
-// export function Header() {
-//     const[menu2, setMenu2]= useState(true);
-//     const[menu1, setMenu1]= useState(false);
-//     const menu2Handler=() =>{
-//         setMenu2(false);
-//         setMenu1(true)
-//     }
-//     const menu1Handler=() =>{
-//         setMenu1(false);
-//         setMenu2(true)
-//     }
-    
-//     const navigate = useNavigate();
-
-//     const handleNavigation = (path: string) => {
-//         navigate(path);
-//     };
-    
-//     return (
-//         <div className={styles.header}>
-//             <ul className={styles.headerMenuList}>
-//                 <li className={styles.headerList} onClick={() => handleNavigation('/homePage')}>
-//                     <img className={styles.headerLogo} src={logo} alt=""/>
-//                 </li>
-//                 <li className={styles.headerList}>
-//                     <img className={styles.headerListIcon} src={phoneIcon} alt=""/>
-//                     <p className={styles.headerListText}>8(800)535-35-35</p>
-//                 </li>
-//                 {/*TODO: добавить логику, если авторизован, то переход не на страницу авторизации, а на профиль*/}
-//                 <li className={styles.headerList} onClick={() => handleNavigation('/auth')}>
-//                     <img className={styles.headerListIcon} src={authorizationIcon} alt=""/>
-//                     <p className={styles.headerListText}>Авторизация</p>
-//                 </li>
-//                 {menu1 &&<li className={styles.headerList} onClick={menu1Handler}>
-//                     <div className={`${styles.headerMenu} ${menu1 ? styles.menuActive : ''}`}>
-//                         <img className={styles.headerListIcon_Exit} src={menuExit} alt=""/>
-//                         <p className={styles.menuItem} onClick={() => handleNavigation('/HomePage')}>Главная</p>
-//                         <p className={styles.menuItem} onClick={() => handleNavigation('/lkClient')}>Личный кабинет</p>
-//                         <p className={styles.menuItem} onClick={() => handleNavigation('/bookingPage')}>Бронирование авто</p>
-//                         </div>
-//                     </li>
-//                 }
-//                 {menu2 && <li className={styles.headerList} onClick={menu2Handler}>
-//                         <img className={styles.headerListIcon} src={menuIcon} alt=""/>
-//                         <p className={styles.headerListText}>Меню</p>
-//                      </li>}
-//             </ul>
-//         </div>
-//     )
-// }
-
-// export default Header;
-
-
 function Overlay({ onClick }: { onClick: () => void }) {
     return <div className={styles.overlay} onClick={onClick}></div>;
 }
