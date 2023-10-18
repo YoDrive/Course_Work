f7952e1d1854fc9a89bd86dedcd1392a6c819153
Daria Platonova <darjac6463@gmail.com>
2023-10-18 13:36:08 +0300

    Свайпер главной страницы

--- yo_drive_client/src/components/homePage/autopark/autopark.tsx
+++ yo_drive_client/src/components/homePage/autopark/autopark.tsx
@@ -1,13 +1,68 @@
 import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import {EffectCoverflow, Navigation} from 'swiper/modules';
+import { CarBookingModel } from '../../../models/Swiper/CarSwiperModel';
+
 import styles from './autopark.module.css';
+import car from '../../../assets/car1.png';
+
+import 'swiper/css';
+import 'swiper/css/effect-coverflow';
+import 'swiper/css/navigation';
+
+let cars: CarBookingModel[] =
+[
+    {
+        id: 1,
+        img: car,
+        name: 'Mercedes-Benz G63 AMG'
+    },
+    {
+        id: 2,
+        img: car,
+        name: 'dkjkf'
+    },
+    {
+        id: 3,
+        img: car,
+        name: 'KDLLK KLDA'
+    },
+    {
+        id: 4,
+        img: car,
+        name: 'KLSKdkd LKDSKLlL'
+    }
+]
 
 export function Autopark() {
 
     return (
         <div className={styles.autoparkContainer}>
             <h2 className={styles.tytle}>Наш автопарк</h2>
-            {/*TODO: слайдер*/}
-
+            <div className={styles.swiperContainer}>
+                <Swiper 
+                modules={[EffectCoverflow, Navigation]}
+                effect={'coverflow'}
+                centeredSlides={true}
+                loop={true}
+                slidesPerView={2}
+                coverflowEffect={{
+                    rotate: 0,
+                    stretch: 40,
+                    depth: 50,
+                    modifier: 4.5,
+                    scale: 1,
+                    slideShadows: false 
+                }}
+                navigation = {true}>
+                {cars.map((car) => (
+                    <SwiperSlide key={car.id}>
+                        <img src={car.img} className={styles.img} alt="slide_image" />
+                        <p className={styles.carName}>{car.name}</p>
+                    </SwiperSlide>
+                ))}
+                </Swiper>
+            </div>
         </div>
     );
 }
