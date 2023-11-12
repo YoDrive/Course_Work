6a411ff9ea02fadc2134ded6b01a2054bd4788c7
AsdZy <99485278+DmitryIldyukov@users.noreply.github.com>
2023-11-12 12:02:35 +0300

    Прописал отправку среднего рейтинга автомобиля

--- YoDrive/Domain/Dtos/CarDto/CarReadDto.cs
+++ YoDrive/Domain/Dtos/CarDto/CarReadDto.cs
@@ -1,7 +1,9 @@
 using YoDrive.Domain.Dtos.CarClassDto;
 using YoDrive.Domain.Dtos.FilialDto;
 using YoDrive.Domain.Dtos.ModelDto;
+using YoDrive.Domain.Dtos.RentDto;
 using YoDrive.Domain.Enums;
+using YoDrive.Domain.Models;
 
 namespace YoDrive.Domain.Dtos.CarDto;
 
@@ -11,8 +13,10 @@ public class CarReadDto
     public CarModelReadDto CarModel { get; set; }
     public ClassReadDto CarClass { get; set; }
     public FilialReadDto Filial { get; set; }
+    public IEnumerable<RentReadDto>? Rents { get; set; }
     public int Year { get; set; }
     public string StateNumber { get; set; }
+    public double Rating { get; set; } 
     public GearBox GearBox { get; set; }
     public decimal CostDay { get; set; }
     public string? CarImage { get; set; }
