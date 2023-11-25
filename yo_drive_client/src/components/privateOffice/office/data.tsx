d1f2ad1a01c0368f98d2fdda6ddcf1cf5c8f6d6c
tanyginvv <tanyginvv@mail.ru>
2023-11-25 12:40:38 +0300

    сделал возможность редактирования данных и их отправки

--- yo_drive_client/src/components/privateOffice/office/data.tsx
+++ yo_drive_client/src/components/privateOffice/office/data.tsx
@@ -1,39 +1,69 @@
 import React, { useState} from 'react';
+import { useForm} from "react-hook-form"
 import styles from './office.module.css';
-//import { User } from '../../../models/Auth/User.model';
+import { UserModel } from '../../../models/Booking/BookingResponseModel';
 export function Data(){
-   // let testModels: BookingResponseModel[] = []
+    let testModels: UserModel[] = [
+        {
+            firstName: "Димочка",
+            surname: "Ильдюков",
+            patronymic: "Юрьевич",
+            email: "asdzy@mail.ru",
+            phoneNumber: "iueffe"
+        }
+    ]
+    const {
+        register,
+        handleSubmit,
+    } = useForm({
+        mode: "onBlur"
+    });
     const [form, setForm] = useState(false);
     const setUserForm =()=>{
         setForm(!form)
     }
+    const onSubmit=(data:any) =>{
+        console.log(data);
+        setUserForm()
+    }
     return(
         <div className={styles.info}>
             <h1 className={styles.infoHeader}>Мои данные</h1>
-            <div className={styles.infoText}>
+            <form className={styles.infoText} onSubmit={handleSubmit(onSubmit)}>
                 <div className={styles.infoName}>
                     <h2 className={styles.name}>Фамилия</h2>
-                    <p className ={styles.nameInfo}>Ильдюков</p>
+                    {!form &&<p className ={styles.nameInfo}>Ильдюков</p>}
+                    {form && <input className={styles.nameInput} defaultValue={"Ильдюков"}
+                    { ...register("firstName")}/>}
                 </div>
                 <div className={styles.infoName}>
                     <h2 className={styles.name}>Имя</h2>
-                    <p className ={styles.nameInfo}>Дмитрий</p>
+                    {!form &&<p className ={styles.nameInfo}>Дмитрий</p>}
+                    {form && <input className={styles.nameInput} defaultValue={"Дмитрий"}
+                    {...register("surName")}/>}
                 </div>
                 <div className={styles.infoName}>
                     <h2 className={styles.name}>Отчество</h2>
-                    <p className ={styles.nameInfo}>Юрьевич</p>
+                    {!form &&<p className ={styles.nameInfo}>Юрьевич</p>}
+                    {form && <input className={styles.nameInput} defaultValue={"Юрьевич"}
+                    {...register("patronymic")}/>}
                 </div>
                 <div className={styles.infoName_phone}>
                     <h2 className={styles.name}>Телефон</h2>
-                    <p className ={styles.nameInfo}>+79023493444</p>
+                    {!form &&<p className ={styles.nameInfo}>+79023493444</p>}
+                    {form && <input className={styles.nameInput} defaultValue={"+79023493444"}
+                    {...register("phoneNumber")}/>}
                 </div>
                 <div className={styles.infoName}>
                     <h2 className={styles.name}>Почта</h2>
-                    <p className ={styles.nameInfo}>asdzy@mail.ru</p>
+                    {!form &&<p className ={styles.nameInfo}>asdzy@mail.ru</p>}
+                    {form && <input className={styles.nameInput} defaultValue={"asdzy@mail.ru"}
+                    {...register("email")}/>}
                 </div>
-            </div>
+            </form>
             <div className={styles.buttons}>
-                <button className={styles.buttonEdit}>Редактировать данные</button>
+                {!form &&<button className={styles.buttonEdit} onClick={()=>setUserForm()}>Редактировать данные</button>}
+                {form &&<button className={styles.buttonEdit} onClick={(handleSubmit(onSubmit))}>Сохранить данные</button>}
                 <button className={styles.buttonImg}>Загрузить изображение</button>
                 <button className={styles.buttonDelete}>Удалить аккаунт</button>
             </div>
