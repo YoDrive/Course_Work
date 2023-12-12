019fd00c25d439346f64684fc15ec2e64e3d7b07
AsdZy <99485278+DmitryIldyukov@users.noreply.github.com>
2023-12-12 23:07:28 +0300

    Вынес карточки машин в отдельный компонент

---
+++ yo_drive_client/src/components/bookingPage/CarCard/сarCard.tsx
@@ -0,0 +1,66 @@
+import React, {FunctionComponent, useState} from 'react';
+import {CarViewModel} from "../../../models/Booking/CarBookingModel";
+import emptyImageCar from "../../../assets/emptyImageCar.png";
+import styles from "./сarCard.module.css";
+import {Rating} from "react-simple-star-rating";
+import FeedbackPopup from "../Popup/bookingPageFeedbackPopup";
+import {GearBoxEnum} from "../../../models/CarModel";
+import BookingPopup from "../Popup/bookingPagePopup";
+
+interface CarCardProps {
+    car: CarViewModel;
+}
+
+const CarCard : FunctionComponent<CarCardProps> = (props) => {
+    const { car } = props;
+    const [openCarIds, setOpenCarIds] = useState<{ [carId: number]: boolean }>({});
+    const [openCarId, setOpenCarId] = useState<number | null>(null);
+
+    const toggleFeedbackPopup = (carId: number) => {
+        setOpenCarIds((prevOpenCarIds) => {
+            const isOpen = prevOpenCarIds[carId];
+            return { ...prevOpenCarIds, [carId]: !isOpen };
+        });
+    };
+
+    const togglePopup = (carId: number) => {
+        if (openCarId === carId) {
+            setOpenCarId(null);
+        } else {
+            setOpenCarId(carId);
+        }
+    }
+
+    return(
+        <>
+            {car.image && (
+                <img
+                    src={`data:image/png;base64,${car.image}`}
+                    alt={`${car.carModel.modelName}`}
+                    width={'452px'} height={'194px'}
+                />
+            )}
+            {!car.image && <img src={emptyImageCar} width={'452px'} height={'194px'}/>}
+            <div className={styles.itemConteiner}>
+                <div className={styles.catalogItemInfo}>
+                    <p className={styles.carName}>{car.carModel.carBrand.name + " " + car.carModel.modelName}</p>
+                    <button className={styles.carStars} onClick={() => toggleFeedbackPopup(car.carId)}>
+                        <Rating size={22} readonly initialValue={car.rating} allowFraction fillColor="#CCB746" emptyColor="#D9D9D9" SVGstrokeColor="#CCB746" SVGstorkeWidth={1}/>
+                        <p className={styles.carStarsNumber}>{car.rating}</p>
+                    </button>
+                    <FeedbackPopup isOpen={openCarIds[car.carId] || false} handleClose={() => toggleFeedbackPopup(car.carId)} carId={car.carId} />
+                    <p className={styles.carYear}>{car.year} год выпуска</p>
+                    <p className={styles.carBox}>{GearBoxEnum[car.gearBox]} коробка передач</p>
+                    <p className={styles.carClass}>Тип кузова: {car.carClass.className}</p>
+                </div>
+                <div className={styles.itemConteinerPrice}>
+                    <p className={styles.carPrice}>{car.costDay.toLocaleString('ru-RU')}₽/сутки</p>
+                    <button className={styles.carButton} onClick={() => togglePopup(car.carId)}>Забронировать</button>
+                    <BookingPopup carId={car.carId} isOpen={openCarId === car.carId} handleClose={() => togglePopup(car.carId)} />
+                </div>
+            </div>
+        </>
+    )
+};
+
+export default CarCard;
--- yo_drive_client/src/components/bookingPage/bookingPage.tsx
+++ yo_drive_client/src/components/bookingPage/bookingPage.tsx
@@ -4,24 +4,18 @@ import styles from './bookingPage.module.css';
 import Header from '../header/header';
 import FilterPanel from './filterPanel/filterPanel';
 import {CarViewModel} from '../../models/Booking/CarBookingModel';
-import { GearBoxEnum } from "../../models/CarModel";
 import galOpen from "../../assets/whgaloshkaClose.svg";
 import galClose from "../../assets/whgalochkaOpen.svg"
 import rows from "../../assets/rows.svg"
-import BookingPopup from './Popup/bookingPagePopup';
-import FeedbackPopup from './Popup/bookingPageFeedbackPopup'
 import 'react-date-range/dist/styles.css'; 
 import 'react-date-range/dist/theme/default.css'
 import BookingService from "../../services/BookingService";
-import { Rating } from 'react-simple-star-rating';
-import emptyImageCar from '../../assets/emptyImageCar.png';
+import CarCard from "./CarCard/сarCard";
 
 export function BookingPage() {
-    const [openCarId, setOpenCarId] = useState<number | null>(null);
     const [cars, setCars] = useState<CarViewModel[] | undefined>([]);
     const [selected, setSelected] = useState(galOpen);
     const [isExpanded, setExpanded] = useState(false);
-    const [openCarIds, setOpenCarIds] = useState<{ [carId: number]: boolean }>({});
 
     const {
         register,
@@ -48,53 +42,13 @@ export function BookingPage() {
         fetchCars();
     }, []);
 
-    const togglePopup = (carId: number) => {
-      if (openCarId === carId) {
-        setOpenCarId(null);
-      } else {
-        setOpenCarId(carId);
-      }
-    }
-
     const toggleExpand = () => {
         setExpanded(!isExpanded);
     };
 
-    const toggleFeedbackPopup = (carId: number) => {
-        setOpenCarIds((prevOpenCarIds) => {
-            const isOpen = prevOpenCarIds[carId];
-            return { ...prevOpenCarIds, [carId]: !isOpen };
-        });
-    };
-
     let listItems = cars?.map((car) =>
         <li key={car.carId} className={styles.catalogItem}>
-            {car.image && (
-                <img
-                    src={`data:image/png;base64,${car.image}`}
-                    alt={`${car.carModel.modelName}`}
-                    width={'452px'} height={'194px'}
-                />
-            )}
-            {!car.image && <img src={emptyImageCar} width={'452px'} height={'194px'}/>}
-            <div className={styles.itemConteiner}>
-                <div className={styles.catalogItemInfo}>
-                    <p className={styles.carName}>{car.carModel.carBrand.name + " " + car.carModel.modelName}</p>
-                    <button className={styles.carStars} onClick={() => toggleFeedbackPopup(car.carId)}>
-                        <Rating size={22} readonly initialValue={car.rating} allowFraction fillColor="#CCB746" emptyColor="#D9D9D9" SVGstrokeColor="#CCB746" SVGstorkeWidth={1}/>
-                        <p className={styles.carStarsNumber}>{car.rating}</p>
-                    </button>
-                    <FeedbackPopup isOpen={openCarIds[car.carId] || false} handleClose={() => toggleFeedbackPopup(car.carId)} carId={car.carId} />
-                    <p className={styles.carYear}>{car.year} год выпуска</p>
-                    <p className={styles.carBox}>{GearBoxEnum[car.gearBox]} коробка передач</p>
-                    <p className={styles.carClass}>Тип кузова: {car.carClass.className}</p>
-                </div>
-                <div className={styles.itemConteinerPrice}>
-                    <p className={styles.carPrice}>{car.costDay.toLocaleString('ru-RU')}₽/сутки</p>
-                    <button className={styles.carButton} onClick={() => togglePopup(car.carId)}>Забронировать</button>
-                    <BookingPopup carId={car.carId} isOpen={openCarId === car.carId} handleClose={() => togglePopup(car.carId)} />
-                </div>
-            </div>
+            <CarCard car={car}/>
         </li>
     );
 
