d41195f2c67783b32e98097945df8e74c3f1ade3
tanyginvv <tanyginvv@mail.ru>
2024-01-04 01:53:50 +0300

    делаю выбор месяца в истории бронирований

--- yo_drive_client/src/components/privateOffice/office/story.tsx
+++ yo_drive_client/src/components/privateOffice/office/story.tsx
@@ -1,15 +1,36 @@
 import styles from './story.module.css';
+import {useState} from 'react';
 import vector from '../../../assets/Vector2.svg'
 import { StoryBlock } from './storyBlock';
+import { Calendar } from 'primereact/calendar';
+import 'primereact/resources/primereact.min.css';
 
 export function Story(){
+    const [selectedMonth, setSelectedMonth] = useState(getCurrentMonth());
+
+    function getCurrentMonth() {
+        const currentDate = new Date();
+        const year = currentDate.getFullYear();
+        let month = currentDate.getMonth() + 1; // Месяцы в JavaScript начинаются с 0
+        return `${year}-${month.toString().padStart(2, '0')}`;
+      }
+
+  const handleMonthChange = (event:any) => {
+    setSelectedMonth(event.target.value);
+  };
+    const[date, setDate]= useState();
+    const dateHandler =()=>{
+        setDate(date);
+        console.log(date);
+    }
     return(
         <div className={styles.info}>
             <div className={styles.headerBlock}>
                <h1 className={styles.infoHeader}>История бронирований</h1>
                <button className={styles.calendar}>
-                    <img className={styles.calendarIcon} src={vector}></img>
-                    <p className={styles.calendarText}>октябрь 2023</p>
+               <input type="month" className={styles.calendarStyles}  defaultValue={selectedMonth}
+          onChange={handleMonthChange} />
+         
                </button>
             </div>
             <div className={styles.infoStory}>
