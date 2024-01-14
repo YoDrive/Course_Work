53a0981b68b58ca0baadd0ab6bb8f622f2012c15
AsdZy <99485278+DmitryIldyukov@users.noreply.github.com>
2024-01-14 14:34:44 +0300

    Добавил попап подтверждения без стилей

---
+++ yo_drive_client/src/components/extentions/successPopUp.tsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react';
+import styles from './successPopUp.module.css';
+import Store from "../../store/Store";
+
+interface Props {
+    onConfirm: () => void;
+    text: string;
+    isVisible: boolean;
+    onClose: () => void;
+}
+
+export function SuccessPopUp(props: Props) {
+
+    const handleConfirm = () => {
+        props.onConfirm();
+        props.onClose();
+    };
+
+    const handleCancel = () => {
+        props.onClose();
+    };
+
+    return (
+        <div className={`${styles.container} ${props.isVisible ? styles.visible : styles.hidden}`}>
+            <div className={styles.popupContent}>
+                <p>{props.text}</p>
+                <div className={styles.buttons}>
+                    <button onClick={handleCancel} className={styles.btn}>Нет</button>
+                    <button onClick={handleConfirm} className={styles.btn}>Да</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default SuccessPopUp;
